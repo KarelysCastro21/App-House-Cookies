c78e0949a616bff17ba5934c44126a14e5747b3c
Karelys Castro <129459178+KarelysCastro21@users.noreply.github.com>
2023-09-05 20:30:41 -0300

    arreglo permiso servidor

--- Backend/Models/Server.js
+++ Backend/Models/Server.js
@@ -35,7 +35,7 @@ class Server {
     middlewares() {
 
         this.app.use(cors({
-            origin: ['http://localhost:3000', 'https://app-house-cookies-arg.vercel.app'], // Cambia esto por la URL de tu frontend
+            origin: 'https://app-house-cookies-arg.vercel.app', // Cambia esto por la URL de tu frontend
             methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
             credentials: true,
         }));
