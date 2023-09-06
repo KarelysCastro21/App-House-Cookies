da2b7a5b6226706c5751e8d9a7c640a5e166a8be
Karelys Castro <129459178+KarelysCastro21@users.noreply.github.com>
2023-09-05 23:42:17 -0300

    confi uri bd

--- Backend/Database/database.js
+++ Backend/Database/database.js
@@ -1,21 +1,18 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-const dbConnection = async ()=>{
-
-    try{
-        await mongoose.connect(process.env.CONNECTION_STRING, {
+const dbConnection = async () => {
+    try {
+        const connectionString = `mongodb+srv://karelysDev:${process.env.CONNECTION_STRING}@cluster0.mmhf5nk.mongodb.net/`;
+        await mongoose.connect(connectionString, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
-        })
+        });
         console.log('Base de datos online');
-       
-    }catch (error) {
+    } catch (error) {
         console.log(error);
         throw new Error('Error al iniciar base de datos');
-
-
     }
 };
 
-module.exports= dbConnection;
+module.exports = dbConnection;
